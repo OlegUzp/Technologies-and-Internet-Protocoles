02e22c5abf783192026036dd1cadac2f1920f904
OlegUzp <29oleg007@gmail.com>
2024-02-19 10:28:01 +0200

    lab2

--- lab2/webpack-project/webpack.config.js
+++ lab2/webpack-project/webpack.config.js
@@ -1,12 +1,16 @@
 // webpack.config.js
 const path = require('path')
+const {CleanWebpackPlugin}=require('clean-webpack-plugin');
 module.exports = {
  mode: "development",
  entry: {
-  main: path.resolve(__dirname, './src/index.js'),
+  main: path.resolve(__dirname, './src/pages'),
  },
  output: {
   path: path.resolve(__dirname, './dist'),
   filename: '[name].bundle.js',
- }, 
+ },
+  plugins: [
+	new CleanWebpackPlugin(),
+	]
 } 
